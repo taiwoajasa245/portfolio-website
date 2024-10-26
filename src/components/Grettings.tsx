6c155685297cc21dd87e8f380d6bb10b4587b5c5
Ajasa Taiwo <ajasataiwo45@gmail.com>
2024-10-26 10:21:14 +0100

    initial commit

--- src/components/Grettings.tsx
+++ src/components/Grettings.tsx
@@ -5,9 +5,9 @@ export default function Grettings() {
 
   return (
     <>
-      <div className="ml-5 mt-16 overflow-auto h-full">
-        <h1>Hello! I am 👋🏽 </h1>
-        <h1 className="text-[35px] tracking-widest">Ajasa Taiwo</h1>
+      <div className="ml-5 mt-16 overflow-auto h-full ">
+        <h1 className="text-[#FDFDFD]">Hello! I am 👋🏽 </h1>
+        <h1 className="text-[35px] text-[#FDFDFD] tracking-widest">Ajasa Taiwo</h1>
         <p className="text-[#79A0BC]">I am a backend developer</p>
 
         <DownloadResume />
