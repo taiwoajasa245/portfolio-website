4b63d51c7dc65f02a8c6c42ba6a56fc41de3bae9
Ajasa Taiwo <ajasataiwo45@gmail.com>
2024-10-28 00:48:21 +0100

    initial commit

--- src/components/NumberLIst.tsx
+++ src/components/NumberLIst.tsx
@@ -6,7 +6,7 @@ export default function NumbersList() {
       {numbers.map((num) => (
         <div
           key={num}
-          className=" h-full text-[#343434] text-[16px] md:text-[10px]"
+          className=" h-full text-[#343434] text-[10px] md:text-[10px]"
         >
           <p>{num}</p>
         </div>
